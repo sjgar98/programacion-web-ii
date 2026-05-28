974987dab80cc69729066fb444ae0ade5dcf724f
FedericoCurutchet <114716003+FedericoCurutchet@users.noreply.github.com>
2026-05-28 18:33:07 -0300

    fix: cambios en las rutas (#18)
    * cambios en las rutas
    
    habia un problema con las rutas  y como buscaba el autoloader las cosas y lo cambie para que funcione en windows y linux, prueben si a usd les anda

--- shared/services/Autoloader.php
+++ shared/services/Autoloader.php
@@ -1,9 +1,8 @@
 <?php
-
 spl_autoload_register(function ($class) {
   $dirs = ['shared/interfaces', 'shared/services', 'controller', 'model', 'config'];
   foreach ($dirs as $dir) {
-    $file = __DIR__ . "/../$dir/$class.php";
+    $file = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $class . '.php';
     if (file_exists($file)) {
       require_once $file;
       return;
