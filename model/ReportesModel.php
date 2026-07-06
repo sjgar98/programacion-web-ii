2aecc69d8f6bde0fca4ba42edffe5e64f3088d53
Santiago Garcia <54064646+sjgar98@users.noreply.github.com>
2026-07-06 16:45:25 -0300

    feat: redirigir a modificar pregunta desde reporte (#76)

--- model/ReportesModel.php
+++ model/ReportesModel.php
@@ -19,7 +19,7 @@ class ReportesModel
     public function obtenerRegistrosReportes()
     {
 
-        $sql = "SELECT r.id, u.username AS nombre_jugador, p.enunciado AS enunciado_pregunta, r.texto,  r.fecha 
+        $sql = "SELECT r.id, p.id as pregunta_id, u.username AS nombre_jugador, p.enunciado AS enunciado_pregunta, r.texto,  r.fecha 
         FROM reportes r
         JOIN usuarios u ON r.jugador_id = u.id
         JOIN preguntas p ON r.pregunta_id = p.id
@@ -30,10 +30,24 @@ class ReportesModel
         return !empty($resultado) ? $resultado : null;
     }
 
+    public function getReporteById(int $reporteId)
+    {
+        if (!$reporteId) return null;
+        $sql = "SELECT * FROM reportes WHERE id = ?";
+        $resultado = $this->database->query($sql, [$reporteId], true);
+        return !empty($resultado) ? $resultado[0] : null;
+    }
+
     public function borrarReporte($id_reporte)
     {
 
         $sql = "DELETE FROM reportes WHERE id = ?";
+        $this->database->execute($sql, [$id_reporte]);
+    }
+
+    public function resolverReporte($id_reporte)
+    {
+        $sql = "UPDATE reportes SET resuelto = 1 WHERE id = ?";
         $this->database->execute($sql, [$id_reporte]);
     }
 }
